27339fab8098029b6eca352af1a84da9e70f6be9
brian <bjbesaw@outlook.com>
2017-09-21 12:41:58 -0400

    user schema and encryption methods added to models

--- models.js
+++ models.js
@@ -1,3 +1,6 @@
+'use strict';
+
+const bcrypt = require('bcryptjs');
 const mongoose = require('mongoose');
 
 const blogPostSchema = mongoose.Schema({
@@ -23,8 +26,33 @@ blogPostSchema.methods.apiRepr = function() {
     title: this.title,
     created: this.created
   };
-}
+};
+
+const UserSchema = mongoose.Schema({
+  username: {type: String, required: true},
+  firstName: String,
+  lastName: String,
+  password: {type: String, required: true}
+});
+
+UserSchema.methods.apiRepr = function() {
+  return {
+    username: this.username,
+    firstName: this.firstName,
+    lastName: this.lastName
+  };
+};
+
+UserSchema.methods.validatePassword = function(password) {
+  return bcrypt.compare(password, this.password);
+};
+
+UserSchema.methods.hashPassword = function(password) {
+  return bcrypt.hash(password, 10);
+};
 
 const BlogPost = mongoose.model('BlogPost', blogPostSchema);
+const User = mongoose.model('User', UserSchema);
 
-module.exports = {BlogPost};
+module.exports = {BlogPost, User};
+// {BlogPost{ ....}, User{...}}
